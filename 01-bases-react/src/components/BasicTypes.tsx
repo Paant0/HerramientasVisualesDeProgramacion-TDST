d338d2de05fe1e2c9e6710148b777a1353162c09
Hervin <hervindc15@gmail.com>
2025-08-28 14:43:09 -0500

    feat(Add UseState): tin

--- 01-bases-react/src/components/BasicTypes.tsx
+++ 01-bases-react/src/components/BasicTypes.tsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const BasicTypes = () => {
-    const name: string = "Hervinsito";
-   const age: number = 28;
-   let isActive: boolean = true;
-
+   const name: string = "Hervinsito";
+   const [age, setAge] = useState(19);
+   const [isActive, setIsActive] = useState(true);
   return (
     <>
-    <div>Hello World
-        
-    </div>
-    <div>{" "+name}
+    <h3>Basic Types</h3>
+    {isActive?
+    <ul>
+      <li>Nombre: {name} </li>
+      <li>Edad:  {age} </li>
+      
+      </ul>:
+      
+<p> Usuarios inactivos, lo lamento brother  </p>
+          }
 
-    </div>
-    <div>{" "+age}
+<div>
+        Hello world
+      </div>
+      <div>
+        {name}
+      </div>
+      <div>
+        {" " + age}
+      </div>
+      <div>
+        {" " + isActive}
+      </div>
+    
+    <h2>
+      Modificar tin
+      
+    </h2>
+    <button onClick={() => {
+      setIsActive(!isActive);
+      setAge(20);
+      
+      }
+    }
+    
 
-    </div>
-    <div>{" "+isActive}
-
-    </div>
+>Modificar</button>
 
     </>
   )
