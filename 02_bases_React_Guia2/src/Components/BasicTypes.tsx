edbed8afbf98cecffd7e6b6243322716938cbc1a
Ponqueew <hervindc15@gmail.com>
2025-09-09 17:05:19 -0500

    feat: add BasicTypes component and update package dependencies

--- 02_bases_React_Guia2/src/Components/BasicTypes.tsx
+++ 02_bases_React_Guia2/src/Components/BasicTypes.tsx
@@ -4,6 +4,15 @@ const name: string= "yo";
 const age: number= 19;
 const [isActive, setisActive] = useState(true);
 
+const names= [
+"alberto romanio1",
+"alberto romanio2",
+"alberto romanio3",
+"alberto romanio4",
+"alberto romanio5",  
+"alberto romanio6",
+];
+
   return (
     <>
     <div>Hola Mundo</div>
@@ -14,7 +23,17 @@ const [isActive, setisActive] = useState(true);
         <li><b>Boleano </b><span>{isActive?"UsuarioActivo":"UsuarioInactivo"}</span></li>
         <button onClick={()=> setisActive(!isActive)}>{isActive?"Desactivar":"Activar"}</button>
     </ul>
+
+    {names[0]}
+
+    {names.map((name, index)=>
+    <li key={index}>
+      {name}
+    </li>
+
+)}
     </>
     
+
   )
 }
