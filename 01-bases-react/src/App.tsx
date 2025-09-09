b8e47c4f3d14aa86be46d4a83d70c96f37eac1f2
Ponqueew <hervindc15@gmail.com>
2025-09-09 16:04:02 -0500

    feat(frontend): cambios-en-app.txt

--- 01-bases-react/src/App.tsx
+++ 01-bases-react/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Ejercicio3 } from './Cajas_Hervin_Actividad_2/Ejercicio3'
+import { Ejercicio1 } from './Cajas_Hervin_Actividad_2/Ejercicio1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Ejercicio3/>
+    <Ejercicio1/>
     </>
   )
 }
